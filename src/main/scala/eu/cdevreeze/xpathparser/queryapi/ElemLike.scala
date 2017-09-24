a27438b3014cdaef8459f1661545c7a109ad519c
Chris de Vreeze <chris.de.vreeze@caiway.net>
2017-09-24 18:20:08 +0200

    Adapted type parameter constraint in ElemLike trait

--- src/main/scala/eu/cdevreeze/xpathparser/queryapi/ElemLike.scala
+++ src/main/scala/eu/cdevreeze/xpathparser/queryapi/ElemLike.scala
@@ -24,7 +24,7 @@ import scala.reflect.ClassTag
  *
  * @author Chris de Vreeze
  */
-trait ElemLike[E <: ElemApi[E]] extends ElemApi[E] { self: E =>
+trait ElemLike[E <: ElemLike[E]] extends ElemApi[E] { self: E =>
 
   import ElemApi.anyElem
 
