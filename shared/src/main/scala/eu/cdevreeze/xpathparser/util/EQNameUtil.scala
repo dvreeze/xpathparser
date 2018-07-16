ccbc46b8e929b8b463b100d6398d48a3bb0bebd1
Chris de Vreeze <chris.de.vreeze@caiway.net>
2018-07-16 23:12:21 +0200

    Improved EQNameUtil and added test

--- shared/src/main/scala/eu/cdevreeze/xpathparser/util/EQNameUtil.scala
+++ shared/src/main/scala/eu/cdevreeze/xpathparser/util/EQNameUtil.scala
@@ -18,6 +18,7 @@ package eu.cdevreeze.xpathparser.util
 
 import scala.reflect.classTag
 
+import eu.cdevreeze.xpathparser.ast.ArgumentList
 import eu.cdevreeze.xpathparser.ast.AtomicOrUnionType
 import eu.cdevreeze.xpathparser.ast.AttributeNameAndTypeTest
 import eu.cdevreeze.xpathparser.ast.AttributeNameTest
@@ -27,6 +28,7 @@ import eu.cdevreeze.xpathparser.ast.EQNameAsArrowFunctionSpecifier
 import eu.cdevreeze.xpathparser.ast.ElementNameAndTypeTest
 import eu.cdevreeze.xpathparser.ast.ElementNameTest
 import eu.cdevreeze.xpathparser.ast.ElementTypeTest
+import eu.cdevreeze.xpathparser.ast.ExprSingleArgument
 import eu.cdevreeze.xpathparser.ast.FunctionCall
 import eu.cdevreeze.xpathparser.ast.NamedFunctionRef
 import eu.cdevreeze.xpathparser.ast.NillableElementNameAndTypeTest
@@ -37,9 +39,13 @@ import eu.cdevreeze.xpathparser.ast.PotentiallyEmptySingleType
 import eu.cdevreeze.xpathparser.ast.SchemaAttributeTest
 import eu.cdevreeze.xpathparser.ast.SchemaElementTest
 import eu.cdevreeze.xpathparser.ast.SimpleNameTest
-import eu.cdevreeze.xpathparser.ast.VariableBinding
+import eu.cdevreeze.xpathparser.ast.StringLiteral
 import eu.cdevreeze.xpathparser.ast.VarRef
+import eu.cdevreeze.xpathparser.ast.VariableBinding
+import eu.cdevreeze.xpathparser.ast.XPathElem
 import eu.cdevreeze.xpathparser.ast.XPathExpr
+import eu.cdevreeze.xpathparser.common.PrefixedName
+import eu.cdevreeze.xpathparser.common.QName
 
 /**
  * Utility to query for used EQNames (and namespace prefixes in those EQNames that are QNames).
@@ -52,15 +58,35 @@ import eu.cdevreeze.xpathparser.ast.XPathExpr
 object EQNameUtil {
 
   /**
+   * Finds the namespace prefixes used in QNames in the given XPath expression, taking the extra EQName producer into account.
+   *
+   * Note that this function does not look for namespace prefixes in (prefix) wildcards.
+   *
+   * In other words, returns `findUsedEQNames(expr, extraEQNameProducer).flatMap(n => findPrefix(n))`.
+   */
+  def findUsedPrefixes(expr: XPathExpr, extraEQNameProducer: XPathElem => Option[Set[EQName]]): Set[String] = {
+    val prefixesInEQNames: Set[String] = findUsedEQNames(expr, extraEQNameProducer).flatMap(n => findPrefix(n))
+    prefixesInEQNames
+  }
+
+  /**
    * Finds the namespace prefixes used in QNames in the given XPath expression.
    *
    * Note that this function does not look for namespace prefixes in (prefix) wildcards.
    *
-   * In other words, returns `findUsedEQNames(expr).flatMap(n => findPrefix(n))`.
+   * In other words, returns `findUsedPrefixes(expr, e => None)`.
    */
   def findUsedPrefixes(expr: XPathExpr): Set[String] = {
-    val prefixesInEQNames: Set[String] = findUsedEQNames(expr).flatMap(n => findPrefix(n))
-    prefixesInEQNames
+    findUsedPrefixes(expr, e => None)
+  }
+
+  /**
+   * Finds the EQNames used in the given XPath expression, taking the extra EQName producer into account.
+   */
+  def findUsedEQNames(expr: XPathExpr, extraEQNameProducer: XPathElem => Option[Set[EQName]]): Set[EQName] = {
+    val extraEQNames = expr.findAllElemsOrSelf.flatMap(e => extraEQNameProducer(e).getOrElse(Set.empty)).toSet
+
+    findUsedEQNames(expr).union(extraEQNames)
   }
 
   /**
@@ -72,78 +98,78 @@ object EQNameUtil {
     // EQNames in variable-refs (note it does not matter if the var-ref is free or bound!)
 
     val eqnamesInVarRefs: Set[EQName] =
-      expr.findAllElemsOfType(classTag[VarRef]).map(_.varName).toSet
+      expr.findAllElemsOrSelfOfType(classTag[VarRef]).map(_.varName).toSet
 
     // EQNames in function names
 
     val eqnamesInNamesOfCalledFunctions: Set[EQName] =
-      expr.findAllElemsOfType(classTag[FunctionCall]).map(_.functionName).toSet
+      expr.findAllElemsOrSelfOfType(classTag[FunctionCall]).map(_.functionName).toSet
 
     val eqnamesInNamedFunctionRefs: Set[EQName] =
-      expr.findAllElemsOfType(classTag[NamedFunctionRef]).map(_.functionName).toSet
+      expr.findAllElemsOrSelfOfType(classTag[NamedFunctionRef]).map(_.functionName).toSet
 
     // EQNames in "simple bindings"
 
     val eqnamesInVariableBindings: Set[EQName] =
-      expr.findAllElemsOfType(classTag[VariableBinding]).map(_.varName).toSet
+      expr.findAllElemsOrSelfOfType(classTag[VariableBinding]).map(_.varName).toSet
 
     // EQNames in (names of) params, which concerns inline function expressions
 
     val eqnamesInParams: Set[EQName] =
-      expr.findAllElemsOfType(classTag[Param]).map(_.paramName).toSet
+      expr.findAllElemsOrSelfOfType(classTag[Param]).map(_.paramName).toSet
 
     // EQNames in name tests
 
     val eqnamesInSimpleNameTests: Set[EQName] =
-      expr.findAllElemsOfType(classTag[SimpleNameTest]).map(_.name).toSet
+      expr.findAllElemsOrSelfOfType(classTag[SimpleNameTest]).map(_.name).toSet
 
     // EQNames in arrow function specifiers
 
     val eqnamesInArrowFunctionSpecifiers: Set[EQName] =
-      expr.findAllElemsOfType(classTag[EQNameAsArrowFunctionSpecifier]).map(_.eqName).toSet
+      expr.findAllElemsOrSelfOfType(classTag[EQNameAsArrowFunctionSpecifier]).map(_.eqName).toSet
 
     // EQNames in types
 
     val eqnamesInTypes: Set[EQName] =
-      expr.findAllElemsOfType(classTag[AtomicOrUnionType]).map(_.tpe).toSet
+      expr.findAllElemsOrSelfOfType(classTag[AtomicOrUnionType]).map(_.tpe).toSet
 
     val eqnamesInNonEmptySingleTypes: Set[EQName] =
-      expr.findAllElemsOfType(classTag[NonEmptySingleType]).map(_.name).toSet
+      expr.findAllElemsOrSelfOfType(classTag[NonEmptySingleType]).map(_.name).toSet
 
     val eqnamesInPotentiallyEmptySingleTypes: Set[EQName] =
-      expr.findAllElemsOfType(classTag[PotentiallyEmptySingleType]).map(_.name).toSet
+      expr.findAllElemsOrSelfOfType(classTag[PotentiallyEmptySingleType]).map(_.name).toSet
 
     // EQNames in kind tests
 
     val eqnamesInAttributeNameTests: Set[EQName] =
-      expr.findAllElemsOfType(classTag[AttributeNameTest]).map(_.name).toSet
+      expr.findAllElemsOrSelfOfType(classTag[AttributeNameTest]).map(_.name).toSet
 
     val eqnamesInAttributeTypeTests: Set[EQName] =
-      expr.findAllElemsOfType(classTag[AttributeTypeTest]).map(_.tpe).toSet
+      expr.findAllElemsOrSelfOfType(classTag[AttributeTypeTest]).map(_.tpe).toSet
 
     val eqnamesInAttributeNameAndTypeTests: Set[EQName] =
-      expr.findAllElemsOfType(classTag[AttributeNameAndTypeTest]).flatMap(t => List(t.name, t.tpe)).toSet
+      expr.findAllElemsOrSelfOfType(classTag[AttributeNameAndTypeTest]).flatMap(t => List(t.name, t.tpe)).toSet
 
     val eqnamesInElementNameTests: Set[EQName] =
-      expr.findAllElemsOfType(classTag[ElementNameTest]).map(_.name).toSet
+      expr.findAllElemsOrSelfOfType(classTag[ElementNameTest]).map(_.name).toSet
 
     val eqnamesInElementTypeTests: Set[EQName] =
-      expr.findAllElemsOfType(classTag[ElementTypeTest]).map(_.tpe).toSet
+      expr.findAllElemsOrSelfOfType(classTag[ElementTypeTest]).map(_.tpe).toSet
 
     val eqnamesInElementNameAndTypeTests: Set[EQName] =
-      expr.findAllElemsOfType(classTag[ElementNameAndTypeTest]).flatMap(t => List(t.name, t.tpe)).toSet
+      expr.findAllElemsOrSelfOfType(classTag[ElementNameAndTypeTest]).flatMap(t => List(t.name, t.tpe)).toSet
 
     val eqnamesInNillableElementTypeTests: Set[EQName] =
-      expr.findAllElemsOfType(classTag[NillableElementTypeTest]).map(_.tpe).toSet
+      expr.findAllElemsOrSelfOfType(classTag[NillableElementTypeTest]).map(_.tpe).toSet
 
     val eqnamesInNillableElementNameAndTypeTests: Set[EQName] =
-      expr.findAllElemsOfType(classTag[NillableElementNameAndTypeTest]).flatMap(t => List(t.name, t.tpe)).toSet
+      expr.findAllElemsOrSelfOfType(classTag[NillableElementNameAndTypeTest]).flatMap(t => List(t.name, t.tpe)).toSet
 
     val eqnamesInSchemaElementTests: Set[EQName] =
-      expr.findAllElemsOfType(classTag[SchemaElementTest]).map(_.name).toSet
+      expr.findAllElemsOrSelfOfType(classTag[SchemaElementTest]).map(_.name).toSet
 
     val eqnamesInSchemaAttributeTests: Set[EQName] =
-      expr.findAllElemsOfType(classTag[SchemaAttributeTest]).map(_.name).toSet
+      expr.findAllElemsOrSelfOfType(classTag[SchemaAttributeTest]).map(_.name).toSet
 
     eqnamesInVarRefs
       .union(eqnamesInNamesOfCalledFunctions)
@@ -176,5 +202,19 @@ object EQNameUtil {
       case EQName.URIQualifiedName(_) => None
       case EQName.QName(qn) => qn.prefixOption
     }
+  }
+
+  /**
+   * Extracts an EQName from expressions like `xs:QName('p:nm')`.
+   */
+  val eqnameProducerFromXsQName: XPathElem => Option[Set[EQName]] = {
+    case FunctionCall(
+      EQName.QName(PrefixedName("xs", "QName")),
+      ArgumentList(Vector(ExprSingleArgument(StringLiteral(s))))) =>
+
+      val qn = QName.parse(s)
+      Some(Set(EQName.QName(qn)))
+    case _: XPathElem =>
+      None
   }
 }
---
+++ shared/src/test/scala/eu/cdevreeze/xpathparser/util/EQNameUtilTest.scala
@@ -0,0 +1,212 @@
+/*
+ * Copyright 2011-2017 Chris de Vreeze
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package eu.cdevreeze.xpathparser.util
+
+import eu.cdevreeze.xpathparser.ast.XPathExpr
+import org.scalatest.FunSuite
+
+/**
+ * EQNameUtil test case.
+ *
+ * @author Chris de Vreeze
+ */
+class EQNameUtilTest extends FunSuite {
+
+  import fastparse.all.Parsed
+
+  import eu.cdevreeze.xpathparser.parse.XPathParser.xpathExpr
+
+  test("testPrefixesInSlash") {
+    val exprString = "/"
+
+    val parseResult = xpathExpr.parse(exprString)
+
+    assertSuccess(parseResult)
+
+    assertUsedPrefixes(parseResult.get.value, Set())
+  }
+
+  test("testPrefixesInSimplePathExpr") {
+    val exprString = "/p:a//p:b/p:c//p:d/p:e"
+
+    val parseResult = xpathExpr.parse(exprString)
+
+    assertSuccess(parseResult)
+
+    assertUsedPrefixes(parseResult.get.value, Set("p"))
+  }
+
+  test("testPrefixesInIfExprWithFunctionCalls") {
+    // From the NL taxonomy (NT12)
+
+    val exprString =
+      "if(xff:has-fallback-value(xs:QName('varArc_BalanceSheetVertical_MsgPrecondValueConceptAndNoExistenceConcept1_ResultForTheYear'))) " +
+        "then true() else not(count($varArc_BalanceSheetVertical_MsgPrecondValueConceptAndNoExistenceConcept1_ResultForTheYear) ge 1)"
+
+    val parseResult = xpathExpr.parse(exprString)
+
+    assertSuccess(parseResult)
+
+    assertUsedPrefixes(parseResult.get.value, Set("xff", "xs"))
+  }
+
+  test("testPrefixesInSummation") {
+    // From the NL taxonomy (NT12)
+
+    val exprString =
+      "$varArc_NotesShareCapitalStatementOfChanges_MsgSeparateSumOfMembersOnAbstract1_Abstract_SumOfMembers =  " +
+        "sum($varArc_NotesShareCapitalStatementOfChanges_MsgSeparateSumOfMembersOnAbstract1_Abstract_ChildrenMember) "
+
+    val parseResult = xpathExpr.parse(exprString)
+
+    assertSuccess(parseResult)
+
+    assertUsedPrefixes(parseResult.get.value, Set())
+  }
+
+  test("testPrefixesInIfExprWithFunctionCallsAndStringLiterals") {
+    // From the NL taxonomy (NT12)
+
+    val exprString =
+      "xfi:fact-has-explicit-dimension-value($varArc_DocumentInformation_MsgPrecondExistenceMemberAspect3_AllItems," +
+        "xs:QName('venj-bw2-dim:FinancialStatementsTypeAxis'),xs:QName('venj-bw2-dm:SeparateMember'))"
+
+    val parseResult = xpathExpr.parse(exprString)
+
+    assertSuccess(parseResult)
+
+    assertUsedPrefixes(parseResult.get.value, Set("xfi", "xs"))
+
+    assertUsedPrefixesIncludingFromXsQName(parseResult.get.value, Set("xfi", "xs", "venj-bw2-dim", "venj-bw2-dm"))
+  }
+
+  test("testPrefixesInLetExpr") {
+    // Example from the XPath 3.0 spec
+
+    val exprString =
+      """let $f := function($a) { starts-with($a, "E") }
+        return local:filter(("Ethel", "Enid", "Gertrude"), $f)"""
+
+    val parseResult = xpathExpr.parse(exprString)
+
+    assertSuccess(parseResult)
+
+    assertUsedPrefixes(parseResult.get.value, Set("local"))
+  }
+
+  test("testPrefixesInNonTrivialForExpr") {
+    // Example from https://github.com/Saxonica/XT-Speedo (adapted)
+
+    val exprString = """for $p:w in //fn:text()/fn:tokenize(., '\W+')[.!=''] return fn:lower-case($p:w)"""
+
+    val parseResult = xpathExpr.parse(exprString)
+
+    assertSuccess(parseResult)
+
+    assertUsedPrefixes(parseResult.get.value, Set("p", "fn"))
+  }
+
+  test("testPrefixesInLetExprWithFreeVars") {
+    // Example from the XPath 3.0 spec, adapted to contain some free variables (adapted)
+
+    val exprString =
+      """let $p1:f := function($p2:a) { starts-with($p2:a, $p3:b) }
+        return local:filter(("Ethel", "Enid", "Gertrude", $p4:c, $Q{}d, $Q{http://www.example.org/}e, $p2:a), $p1:f)"""
+
+    val parseResult = xpathExpr.parse(exprString)
+
+    assertSuccess(parseResult)
+
+    assertUsedPrefixes(parseResult.get.value, Set("p1", "p2", "p3", "p4", "local"))
+  }
+
+  test("testPrefixesInExprWithBindingsDependingOnOtherBindings") {
+    // From the NL taxonomy (NT12)
+
+    val exprString =
+      """fn:not((for
+                $n in (1 to (fn:count($varArc_ELRName_PrtFactsNECovA_Set01)-1)),
+                $m in (($n+1) to (fn:count($varArc_ELRName_PrtFactsNECovA_Set01)))
+              return (($varArc_ELRName_PrtFactsNECovA_Set01[$n] = $varArc_ELRName_PrtFactsNECovA_Set01[$m]))) = fn:true())""".trim
+
+    val parseResult = xpathExpr.parse(exprString)
+
+    assertSuccess(parseResult)
+
+    assertUsedPrefixes(parseResult.get.value, Set("fn"))
+  }
+
+  test("testPrefixesInCastExpr") {
+    val exprString = "xs:NCName('entity') cast as xs:ENTITY"
+
+    val parseResult = xpathExpr.parse(exprString)
+
+    assertSuccess(parseResult)
+
+    assertUsedPrefixes(parseResult.get.value, Set("xs"))
+  }
+
+  test("testPrefixesInNamedFunctionRef") {
+    val exprString = "exists(fn:minutes-from-duration#1)"
+
+    val parseResult = xpathExpr.parse(exprString)
+
+    assertSuccess(parseResult)
+
+    assertUsedPrefixes(parseResult.get.value, Set("fn"))
+  }
+
+  test("testPrefixesInSimpleNameTest") {
+    val exprString = "let $var := /works return fn:count($var/child::p:employee)"
+
+    val parseResult = xpathExpr.parse(exprString)
+
+    assertSuccess(parseResult)
+
+    assertUsedPrefixes(parseResult.get.value, Set("fn", "p"))
+  }
+
+  private def assertUsedPrefixes(
+    expr: XPathExpr,
+    expectedPrefixes: Set[String]): Unit = {
+
+    val prefixes = EQNameUtil.findUsedPrefixes(expr)
+
+    assertResult(expectedPrefixes) {
+      prefixes
+    }
+  }
+
+  private def assertUsedPrefixesIncludingFromXsQName(
+    expr: XPathExpr,
+    expectedPrefixes: Set[String]): Unit = {
+
+    val prefixes = EQNameUtil.findUsedPrefixes(expr, EQNameUtil.eqnameProducerFromXsQName)
+
+    assertResult(expectedPrefixes) {
+      prefixes
+    }
+  }
+
+  private def assertSuccess(parseResult: Parsed[_]): Unit = {
+    assertResult(true) {
+      parseResult.fold(
+        (parser, pos, extra) => false,
+        (expr, pos) => true)
+    }
+  }
+}
